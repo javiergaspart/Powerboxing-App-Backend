ebb9873bec8a8751466c9b48e9e364f7c1d5c673
Javier Gaspart <javiergaspart@gmail.com>
2025-02-03 20:08:13 +0100

    Fixed User model and authentication middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,16 +1,20 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 module.exports = function (req, res, next) {
-    const token = req.header('x-auth-token');
+    const token = req.header("x-auth-token");
+
     if (!token) {
-        return res.status(401).json({ message: "Access Denied. No Token Provided." });
+        return res.status(401).json({ message: "Access denied. No token provided." });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        req.user = decoded.user;
         next();
-    } catch (err) {
-        res.status(400).json({ message: "Invalid Token" });
+    } catch (error) {
+        res.status(401).json({ message: "Invalid token" });
     }
 };
--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,28 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["trainer", "boxer"], required: true },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["trainer", "boxer", "admin"],
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
 });
 
 module.exports = mongoose.model("User", UserSchema);
